65ac4d05e875f4b6e1900c4c9c6a5e22255fa690
dmittal1 <mittald@hotmail.co.uk>
2020-09-01 23:18:40 +0100

    Changed File Extension

--- client/src/routes/AppRouter.js
+++ client/src/routes/AppRouter.js
@@ -1,14 +1,21 @@
 import react from 'react';
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
-
+import { register } from '../serviceWorker';
+import Home from '../Pages/Home';
+import document from '../Pages/Documents';
+import profile from '../Pages/Profile';
+import login from '../Pages/Login';
+import register from '../Pages/Register';
 
 const AppRouter = () => (
     <BrowserRouter>
         <div>
             <Switch>
-                <Route path="/" component={} exact={true} />
-                <Route path="/create" component={} />
-
+                <Route path="/" component={Home} exact={true} />
+                <Route path="/document" component={document} />
+                <Route path="/profile" component={profile} />
+                <Route path="/login" component={login} />
+                <Route path="/register" component={register} />
             </Switch>
         </div>
     </BrowserRouter>
